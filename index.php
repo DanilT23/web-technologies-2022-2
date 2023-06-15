2ca9008102aea85b0825de665884de253a15a6bf
Danil Tolkov <daniltolkov@mail.ru>
2023-06-15 17:07:48 +0500

    задание 18 (пока что без 5)

--- index.php
+++ index.php
@@ -1,141 +1,128 @@
 <?php
 $html = file_get_contents("./index.html");
 //Задание 1
-$a = -1;
-$b = 2;
-if($a >= 0 and $b >= 0)
-    $result = "a и b положительные, вывести их разность: " . $a-$b;
-else if($a < 0 and $b < 0)
-    $result = "а и b отрицательные, вывести их произведение: " . $a*$b;
-else
-    $result = "а и b разных знаков, вывести их сумму: " . $a+$b;
+$i = 0;
+$result ='';
+do {
+    if($i % 2 == 1)
+        $result = $result . "$i - нечётное число.</br>";
+    elseif($i % 2 == 0 and $i > 0)
+        $result = $result . "$i - чётное число.</br>";
+    else {
+        $result = $result . "$i - это ноль.</br>";
+    }
+    $i++;
+} while($i < 11);
 $html = str_replace('{{ ex1 }}', $result, $html);
-/*если $a и $b положительные, вывести их разность;
-если $а и $b отрицательные, вывести их произведение;
-если $а и $b разных знаков, вывести их сумму;
-Ноль можно считать положительным числом.*/
+echo $html;
 //Задание 2
-$a = 11;
-$result = '';
-switch ($a){
-    case 0:
-        $result = $result . "0 ";
-    case 1:
-        $result = $result . "1 ";
-    case 2:
-        $result = $result . "2 ";
-    case 3:
-        $result = $result . "3 ";
-    case 4:
-        $result = $result . "4 ";
-    case 5:
-        $result = $result . "5 ";
-    case 6:
-        $result = $result . "6 ";
-    case 7:
-        $result = $result . "7 ";
-    case 8:
-        $result = $result . "8 ";
-    case 9:
-        $result = $result . "9 ";
-    case 10:
-        $result = $result . "10 ";
-    case 11:
-        $result = $result . "11 ";
-    case 12:
-        $result = $result . "12 ";
-    case 13:
-        $result = $result . "13 ";
-    case 14:
-        $result = $result . "14 ";
-    case 15:
-        $result = $result . "15 ";
-}
-$html = str_replace('{{ ex2 }}', $result, $html);
-//Задание 3
-$a = 5;
-$b = 10;
-$result = 'Сумма: ' . Sum($a,$b).
-    '</br>Разность: ' . Dif($a,$b).
-    '</br>Произведение: ' . Multip($a,$b).
-    '</br>Деление: ' . Div($a,$b);
-$html = str_replace('{{ ex3 }}', $result, $html);
 
-function Sum($a,$b){
-    return($a + $b);
+$arr = [];
+$arr = [
+    'Московская область' => ['Москва', 'Зеленоград', 'Клин'],
+    'Ленинградская область' => ['Санкт-Петербург', 'Всеволожск', 'Павловск', 'Кронштадт'],
+    'Рязанская область' => ['Рязань', 'Ряжск', 'Шацк', 'Рыбное', 'Скопин', 'Касимов', 'Михайлов']
+];
+
+foreach ($arr as $key => $city) {
+    echo $key . ':</br>';
+    echo implode(', ', $city) . '.<br>';
 }
-function Dif($a,$b){
-    return($a - $b);
-}
-function Multip($a,$b){
-    return($a * $b);
-}
-function Div($a,$b){
-    return($a / $b);
+
+//Задание 3
+echo "</br> <h2>Задание 3:</h2>";
+$arr =[
+    'a' => 'a',
+    'б' => 'b',
+    'в' => 'v',
+    'г' => 'g',
+    'д' => 'd',
+    'е' => 'e',
+    'ё' => 'yo',
+    'ж' => 'zh',
+    'з' => 'z',
+    'и' => 'i',
+    'й' => 'j',
+    'к' => 'k',
+    'л' => 'l',
+    'м' => 'm',
+    'н' => 'n',
+    'о' => 'o',
+    'п' => 'p',
+    'р' => 'r',
+    'с' => 's',
+    'т' => 't',
+    'у' => 'u',
+    'ф' => 'f',
+    'х' => 'x',
+    'ц' => 'cz',
+    'ч' => 'ch',
+    'ш' => 'sh',
+    'щ' => 'shh',
+    'ъ' => '',
+    'ы' => 'y',
+    'ь' => '',
+    'э' => 'e',
+    'ю' => 'yu',
+    'я' => 'ya'
+];
+$text = 'какой то текст';
+$new_text = convertToTranslit($text,$arr);
+echo "$new_text<br>";
+
+function convertToTranslit($s,$arr){
+    return strtr(mb_strtolower($s), $arr);
 }
 //Задание 4
-$a = 4;
-$b = 4;
-$result = 'Сумма: ' . mathOperation($a,$b,'+').
-    '</br>Разность: ' . mathOperation($a,$b,'-').
-    '</br>Произведение: ' . mathOperation($a,$b,'*').
-    '</br>Деление: ' . mathOperation($a,$b,'/');
-$html = str_replace('{{ ex4 }}', $result, $html);
-function mathOperation($arg1,$arg2,$operation){
-    if($operation == '+')
-        return Sum($arg1,$arg2);
-    if($operation == '-')
-        return Dif($arg1,$arg2);
-    if($operation == '*')
-        return Multip($arg1,$arg2);
-    if($operation == '/')
-        return Div($arg1,$arg2);
-}
-//Задание 5
-$year = date('Текущий год: '.'Y');
-//1 способ
-$html = str_replace('{{ ex5 }}', '1 способ: ' . $year, $html);
-//2 способ
-echo '2 Способ: '.$year;
-//3 способ
-?>
-    <!DOCTYPE html>
-    <html lang="ru">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>{{ title }}</title>
+echo "</br> <h2>Задание 4:</h2>";
 
+$menu = [
+    'Страница',
+    'Меню' => [
+        'Подменю 1',
+        'Подменю 2'
+    ],
+    'Меню 2' => [
+    'Подменю 1',
+    'Подменю 2'
+],
+];
 
-        <link rel="stylesheet">
-        <meta property="og:title" content="Заголовок страницы в OG">
-        <meta property="og:description" content="Описание страницы в OG">
-        <meta property="og:image" content="https://example.com/image.jpg">
-        <meta property="og:url" content="https://example.com/">
-    </head>
-    <body>
-    <header>
-        <h1>Способ 3:</h1>
-        <p><?= $year ?></p>
-        <nav>
-            <ul>
-                <li><a href="index.html">Эта страница</a></li>
-            </ul>
-        </nav>
-    </header>
-    </body>
-    </html>
-<?php
-//Задание 6
-$result = power(4,4);
-$html = str_replace('{{ ex6 }}', $result, $html);
-echo $html;
-function power($val,$pow){
-    if($pow == 0 and $val <> 0)
-        return 1;
-    elseif($pow > 0){
-        return $val*power($val,$pow-1);
+echo '<ul>';
+foreach ($menu as $key => $value) {
+    if (is_array($value)) {
+        echo '<li> <a href="index.html"> ' . $key . ' </a> <ul>';
+        foreach ($value as $new_value) {
+            echo '<li> <a href="index.html"> ' . $new_value . ' </a> </li>';
+        }
+        echo '</ul>' . '</li>';
     }
-    elseif($pow < 0)
-        return 1/$val*power($val,$pow+1);
+    else {
+        echo '<li> <a href="index.html">' . $value . '</a></li>';
+    }
 }
+echo '</ul>';
+
+//Задание 5
+echo "</br> <h2>Задание 5:</h2>";
+
+//Задание 6
+echo "</br> <h2>Задание 6:</h2>";
+
+$arr = [];
+$arr = [
+    'Московская область' => ['Москва', 'Зеленоград', 'Клин'],
+    'Ленинградская область' => ['Санкт-Петербург', 'Всеволожск', 'Павловск', 'Кронштадт'],
+    'Рязанская область' => ['Рязань', 'Ряжск' , 'Шацк', 'Рыбное', 'Скопин', 'Касимов', 'Михайлов']
+];
+foreach ($arr as $key => $value) {
+    if (is_array($value)) {
+        foreach ($value as $new_key => $new_value) {
+            if (mb_substr($new_value, 0,1) == 'К') {
+                echo $new_value.' ';
+            }
+        }
+    }
+}
+echo '<br>';
+
